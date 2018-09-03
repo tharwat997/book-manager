77cd07fabf2ab334131b22912eb7352007396813
YOUSSEF THARWAT MOKHTAR MOHAMED KHALIL <TP040164@mail.apu.edu.my>
2018-09-04 02:11:11 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueFire from 'vuefire'
+
+
+Vue.use(VueFire)
+
+
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
